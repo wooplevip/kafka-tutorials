a150716bd95b0ed40578943ce26f6e644d48c98e
wooplevip <woople@foxmail.com>
2019-10-29 15:31:28 +0800

    Add a consumer simple

---
+++ src/main/java/com/woople/kafka/tutorials/consumer/SimpleConsumer.java
@@ -0,0 +1,34 @@
+package com.woople.kafka.tutorials.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * Created by peng on 2017/2/16.
+ */
+public class SimpleConsumer {
+    private final static Logger logger = LoggerFactory.getLogger(SimpleConsumer.class);
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.setProperty("bootstrap.servers", "10.1.236.66:9092");
+        props.setProperty("group.id", "test");
+        props.setProperty("enable.auto.commit", "true");
+        props.setProperty("auto.commit.interval.ms", "1000");
+        props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("foo", "bar"));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record : records)
+                logger.info("offset = {}, key = {}, value = {}", record.offset(), record.key(), record.value());
+        }
+    }
+}
